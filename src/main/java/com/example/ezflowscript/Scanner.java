a31adbd6abccdf0a8859681e0d3bbe5ad088b9aa
mrokuu <mroczkowskiadammm@gmail.com>
2024-01-25 16:50:21 +0100

    create scanner

--- src/main/java/com/example/ezflowscript/Scanner.java
+++ src/main/java/com/example/ezflowscript/Scanner.java
@@ -6,8 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
 
-
+    Scanner(String source) {
+        this.source = source;
+    }
 
 
 }
