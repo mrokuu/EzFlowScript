57bab57f3577d66719e7109144858dd64736d6c2
mrokuu <mroczkowskiadammm@gmail.com>
2024-01-26 18:10:12 +0100

    expresstion - super

--- src/main/java/com/example/ezflowscript/Expr.java
+++ src/main/java/com/example/ezflowscript/Expr.java
@@ -47,6 +47,20 @@ abstract class Expr {
         }
     }
 
+    static class Super extends Expr {
+        final Token keyword;
+        final Token method;
+
+        Super(Token keyword, Token method) {
+            this.keyword = keyword;
+            this.method = method;
+        }
+
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitSuperExpr(this);
+        }
+    }
+
 
 
 
