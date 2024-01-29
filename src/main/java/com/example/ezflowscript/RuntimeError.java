e5c969e84ca5757f1796a75cb0f52d5d235565bd
mrokuu <mroczkowskiadammm@gmail.com>
2024-01-29 07:39:45 +0100

    working interpreter

---
+++ src/main/java/com/example/ezflowscript/RuntimeError.java
@@ -0,0 +1,10 @@
+package com.example.ezflowscript;
+
+class RuntimeError extends RuntimeException{
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
--- src/main/java/com/example/ezflowscript/TokenType.java
+++ src/main/java/com/example/ezflowscript/TokenType.java
@@ -1,21 +1,46 @@
 package com.example.ezflowscript;
 
 enum TokenType {
-    // Single-Character Tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+    EOF;
 
-    // One or two character Tokens
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
-
-    // Literals
-    IDENTIFIER, STRING, NUMBER,
-
-    // Keywords
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-    EOF
+    private TokenType() {
+    }
 }
